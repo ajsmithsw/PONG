fa67ac17d33025214398eb809e852b7135dffe34
Alex <ajsmithsw@gmail.com>
2015-09-24 20:07:01 +0100

    Update AI.js

--- AI.js
+++ AI.js
@@ -1,45 +1,48 @@
 ﻿#pragma strict
-
+﻿
 var theBall : Transform;
 var balPosY : float;
 var balVelX : float;
 var random : float;
- 
+
 function Start(){
-    //InvokeRepeating("UpdatePos", 1, 0.2);
     theBall = GameObject.FindGameObjectWithTag("Ball").transform;
 }
-
-
-
+ 
 function Update () {
     balPosY = theBall.position.y + random;
     balVelX = theBall.GetComponent.<Rigidbody2D>().velocity.x;
-    
+   
+    // if the ball is moving...
     if (balVelX != 0) {
-	    if(balPosY>GetComponent.<Rigidbody2D>().position.y + 0.2)
-	    {
-	        GetComponent.<Rigidbody2D>().velocity.y = 5;
-	    }
-	    else if(balPosY<GetComponent.<Rigidbody2D>().position.y - 0.2) {
-	        GetComponent.<Rigidbody2D>().velocity.y = -5;
-		}
-		else {
-			GetComponent.<Rigidbody2D>().velocity.y = 0;
-		}
-	}
-	else {
-		if (GetComponent.<Rigidbody2D>().position.y > 0.1f &&
-				GetComponent.<Rigidbody2D>().position.y < -0.1f) {
-			GetComponent.<Rigidbody2D>().velocity.y = -5;
-		} else if (GetComponent.<Rigidbody2D>().position.y < -0.1f &&
-				GetComponent.<Rigidbody2D>().position.y < 0.1f) {
-			GetComponent.<Rigidbody2D>().velocity.y = 5;
-		}
-	
-	}
+        if(balPosY>GetComponent.<Rigidbody2D>().position.y + 0.2)
+        {
+            GetComponent.<Rigidbody2D>().velocity.y = 5;
+        }
+        else if(balPosY<GetComponent.<Rigidbody2D>().position.y - 0.2) {
+            GetComponent.<Rigidbody2D>().velocity.y = -5;
+        }
+        else {
+            GetComponent.<Rigidbody2D>().velocity.y = 0;
+        }
+    }
+    // Get the AI to move back towards the centre when a new round begins
+    else {
+        if (GetComponent.<Rigidbody2D>().position.y > 0.1f &&
+                GetComponent.<Rigidbody2D>().position.y < -0.1f) {
+            GetComponent.<Rigidbody2D>().velocity.y = -5;
+        } else if (GetComponent.<Rigidbody2D>().position.y < -0.1f &&
+                GetComponent.<Rigidbody2D>().position.y < 0.1f) {
+            GetComponent.<Rigidbody2D>().velocity.y = 5;
+        }
+   
+    }
 }
-
+ 
+// this next function is called from the Ball script, each time the ball collides
+// with a player (it was too much to call it 60 times per second in the Update()
+// function above). This seems to work nicely:
+ 
 function newRandom() {
-	random = Random.Range(-1.5f, 1.5f);
+    random = Random.Range(-1.5f, 1.5f);
 }
